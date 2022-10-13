ced58cced4822e7863f4c42effb0a55f3e0e0612
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-10-13 10:10:28 +0300

    Edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import {HeaderModule} from "./features/header/header.component.module";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from "@angular/material/dialog";
+import {RegistrationModule} from "./features/registration/registration.component.module";
+import {LoginModule} from "./features/login/login.component.module";
+import {BASE_PATH, UserService} from "./core/services/flower-shop";
+import { HttpClientModule} from '@angular/common/http';
+import {environment} from "../environments/environment";
+
+
+
 
 
 @NgModule({
@@ -12,9 +22,19 @@ import {HeaderModule} from "./features/header/header.component.module";
   imports: [
     BrowserModule,
     HeaderModule,
-
+    NgbModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+ RegistrationModule,
+    LoginModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    UserService,{
+      provide: BASE_PATH,
+      useValue: environment.serverUri,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
