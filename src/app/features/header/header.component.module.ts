24ea54ebad3695283ae0e9b1b27a960d46b6f9ff
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-10-13 10:09:32 +0300

    header

--- src/app/features/header/header.component.module.ts
+++ src/app/features/header/header.component.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from "@angular/core";
-import {HeaderComponent} from "./header.component";
+import { HeaderComponent} from "./header.component";
+import {NgbCollapseModule} from "@ng-bootstrap/ng-bootstrap";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule} from "@angular/forms";
+import {MatDialogModule} from "@angular/material/dialog";
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
+
   ],
   imports: [
+    NgbCollapseModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatDialogModule
 
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+
   ],
 })
 export class HeaderModule {
--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, Inject, OnInit} from '@angular/core';
+import {MatDialogModule, MatDialogRef, MAT_DIALOG_DATA, MatDialog} from "@angular/material/dialog";
+import {RegistrationComponent} from "../registration/registration.component";
+import {LoginComponent} from "../login/login.component";
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  animal: string;
+  name: string;
+  constructor(private elementRef: ElementRef,public dialog: MatDialog) {
+
+  }
+  ngOnInit(): void {
+    this.elementRef.nativeElement.querySelectorAll('.nav-item').forEach((element: any) => {
+
+      element.addEventListener('mouseenter', (event: any) => {
+        if (window.innerWidth > 750) {
+          var n= event.target.closest('.nav-item');
+          n.classList.add('show');
+        }
+
+      });
+    });
+
+    this.elementRef.nativeElement.querySelectorAll('.nav-item').forEach((element: any) => {
+
+      element.addEventListener('mouseleave', (event: any) => {
+        if (window.innerWidth > 750) {
+          var n= event.target.closest('.nav-item');
+          if(!n.isHidden){
+            n.classList.remove('show');
+          }
+
+        }
+
+      });
+    });
+  }
+  openRegisterDialog(){
+    const dialogRef = this.dialog.open(RegistrationComponent, {
+     minWidth: '40w',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+
+    });
+  }
+  openLoginDialog(){
+    const dialogRef = this.dialog.open(LoginComponent, {
+      minWidth: '40w',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+
+    });
+  }
+
+
   title = 'FloweShop-Client';
+  public isHidden: boolean=false;
+
+
 }
+
+
