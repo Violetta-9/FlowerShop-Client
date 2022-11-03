929c766b11a41c436851fd889256353b70597140
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-11-03 11:02:57 +0300

    admin page

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from "@angular/core";
+import {FormControl} from "@angular/forms";
+import {map, Observable, startWith} from "rxjs";
+import {AdminService, ProductDTO, ProductService, ProductTitleAndIdDTO} from "../../core/services/flower-shop";
+
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  myControl = new FormControl('');
+  productTitle:ProductTitleAndIdDTO[];
+  filteredOptions: Observable<ProductTitleAndIdDTO[]>;
+  products:Array<ProductDTO>;
+productId:number
+  isCompled=false;
+  @ViewChild("variable",{static:false})
+  nameParagraph:ElementRef|undefined;
+  public constructor(private productServ:ProductService,
+                     private adminService: AdminService) {
 
-  ngOnInit(): void {
   }
 
+  async ngOnInit() {
+    await this.getAllTitle();
+
+
+  }
+
+  private _filter(value: string): ProductTitleAndIdDTO[] {
+    const filterValue = value.toLowerCase();
+
+    return this.productTitle.filter(option => option.title.toLowerCase().includes(filterValue));
+  }
+
+  private getAllTitle() {
+    this.productServ.getProductTitle().subscribe(x=>this.productTitle=x);
+  }
+
+  show() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+    this.isCompled=false;
+  }
+
+  find() {
+ this.productId=Number(this.nameParagraph.nativeElement.textContent)
+    console.log( this.productId);
+    this.isCompled=true;
+  }
 }
+
