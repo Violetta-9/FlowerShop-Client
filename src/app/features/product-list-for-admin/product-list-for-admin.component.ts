b403901c93acd48ec3969c89b0b8fed240ad6c62
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-11-12 13:29:31 +0300

    update product-list-for-admin

--- src/app/features/product-list-for-admin/product-list-for-admin.component.ts
+++ src/app/features/product-list-for-admin/product-list-for-admin.component.ts
@@ -1,6 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {AdminService, ProductDTO} from "../../core/services/flower-shop";
+import {LoginComponent} from "../login/login.component";
+import {MatDialog} from "@angular/material/dialog";
+import {ComfirmDeletComponent} from "../comfirm-delet/comfirm-delet.component";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-product-list-for-admin',
@@ -11,7 +15,9 @@ export class ProductListForAdminComponent implements OnInit {
   @Input() public productId: number;
   product:ProductDTO;
   constructor(private route: ActivatedRoute,
-              private adminService:AdminService) { }
+              private adminService:AdminService,
+              public dialog: MatDialog,
+              private toastr: ToastrService) { }
 
   ngOnInit(): void {
     console.log("ddddd"+this.productId)
@@ -26,5 +32,29 @@ export class ProductListForAdminComponent implements OnInit {
   private getProductById() {
 
     this.adminService.getProductById(this.productId).subscribe(x=>this.product=x);
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(ComfirmDeletComponent, {
+      minWidth: '40w',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result){
+       this.adminService.deleteProduct(544).subscribe(x=>{
+         if(x){
+           this.showSuccess();
+         }
+
+       });
+
+      }
+
+    });
+
+
+  }
+  showSuccess() {
+    this.toastr.success('Продукт успешно удален', 'Успех!');
   }
 }
