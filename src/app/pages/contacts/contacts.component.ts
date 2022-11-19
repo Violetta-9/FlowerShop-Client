941dc02ed46c2e251bae21110e900a666ac8037d
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-11-19 20:26:19 +0300

    contacts

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts.component.css']
 })
 export class ContactsComponent implements OnInit {
-
+public workTime="С 10:00 до 19:00"
+ public weekend="Выходной: суббота, воскресенье";
   constructor() { }
 
   ngOnInit(): void {
